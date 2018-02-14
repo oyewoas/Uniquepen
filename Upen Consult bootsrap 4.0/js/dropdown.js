a8dd8a9439adf525907b9c56e0298e4714ad3ec0
oyewoas <31412106+oyewoas@users.noreply.github.com>
2018-02-14 04:03:34 +0100

    Add files via upload

---
+++ Upen Consult bootsrap 4.0/js/dropdown.js
@@ -0,0 +1,53 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+
+// // When the user scrolls the page, execute myFunction 
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+// $('a#services').on('mouseover', function(){
+//     $(this).siblings('ul.dropdown-menu').removeClass('closed').addClas('open');
+//     });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
---
+++ Upen Consult bootsrap 4.0/js/usingjscpt.js
@@ -0,0 +1,37 @@
+// alert("Hello Samuel")
+// alert("How are you doing")
+// alert("You are welcome")
+// var price = 15.00 //The value of a product
+// var money = 20.00 //The amount i have
+// if (money >= price) {
+// 	console.log ("Buy The hammer");
+// } else {
+// 	console.log ("dont buy it");
+// }
+
+// deciding whether to go to the park with a friend over the weekend
+var colt = "not busy";
+var weather = "nice";
+
+if (colt == "not busy" && weather == "nice") {
+	console.log("go to park");
+}
+
+// while loop
+var x = 1;
+while (x <= 10,000) {
+	console.log(x + "mississippi!");
+	x = x + 1;
+}
+// for loop
+for (var i = 0; i < 6; i = i + 1) {
+	console.log("print out i =" + i)
+}
+// nested loops
+for (var i = 0; i < 3; i= i + 1) {
+	for (var a = 0; a < 2; i = i + 1) {
+		console.log(i + '', '' + a);
+	}
+	
+}
+
